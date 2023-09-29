7b648a2debffe692a840c1f3bb3873bbf942e1c8
ldphat99 <ldphat99@gmail.com>
2023-09-29 16:55:59 +0700

    Add: handle exception for Jakarta Validation

--- src/main/java/com/dandelion/backend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/dandelion/backend/exceptions/GlobalExceptionHandler.java
@@ -4,10 +4,14 @@ package com.dandelion.backend.exceptions;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -38,5 +42,18 @@ public class GlobalExceptionHandler {
                 .build();
 
         return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        Map<String, String> res = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach((err) -> {
+            String fieldName = ((FieldError) err).getField();
+            String message = err.getDefaultMessage();
+
+            res.put(fieldName, message);
+        });
+
+        return new ResponseEntity<Map<String, String>>(res, HttpStatus.BAD_REQUEST);
     }
 }
