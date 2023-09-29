ca1cd014c186ab2311be1d0d9bbc9409dd090209
ldphat99 <ldphat99@gmail.com>
2023-09-29 21:45:13 +0700

    UPDATE: Update validation

--- src/main/java/com/dandelion/backend/payloads/UserDTO.java
+++ src/main/java/com/dandelion/backend/payloads/UserDTO.java
@@ -6,31 +6,33 @@ import com.dandelion.backend.utils.GenderDeserializer;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 @Builder
 public class UserDTO {
 
     private Long id;
 
     @Email(message = "Email address is not valid!")
-    @NotEmpty(message = "Email is not empty!")
+    @NotBlank(message = "Email is not blank!")
     private String email;
 
-    @NotEmpty(message = "Phone is not empty!")
+    @NotBlank(message = "Phone is not blank!")
+    @Size(min = 10, max = 10, message = "Phone must be with 10 characters!")
     private String phone;
 
-    @NotEmpty
+    @NotBlank(message = "Password is not blank!")
     @Size(min = 8, max = 32, message = "Password must be min of 8 characters!")
     private String password;
 
-    @NotEmpty
+    @NotBlank(message = "Name is not blank!")
+    @Size(min = 2, max = 100, message = "Name must be in range of 2-100 characters!")
     @JsonProperty("full_name")
     private String fullName;
 
