37dae1b6304834aee310bcf71f954b0c56737ccf
ldphat99 <ldphat99@gmail.com>
2023-09-30 23:04:04 +0700

    ADD: add Variation and VariationOption Repo

--- src/main/java/com/dandelion/backend/repositories/VariationOptionRepo.java
+++ src/main/java/com/dandelion/backend/repositories/VariationOptionRepo.java
@@ -1,2 +1,16 @@
-package com.dandelion.backend.repositories;public interface VariationOptionRepo {
+package com.dandelion.backend.repositories;
+
+import com.dandelion.backend.entities.VariationOption;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface VariationOptionRepo extends JpaRepository<VariationOption, Long> {
+    List<VariationOption> findByVariation_NameIgnoreCase(String name);
+
+    List<VariationOption> findByVariation_Category_NameIgnoreCase(String name);
+
+    List<VariationOption> findByVariation_NameIgnoreCaseAndVariation_Category_NameIgnoreCase(String name, String name1);
+
+
 }
--- src/main/java/com/dandelion/backend/repositories/VariationRepo.java
+++ src/main/java/com/dandelion/backend/repositories/VariationRepo.java
@@ -1,2 +1,12 @@
-package com.dandelion.backend.repositories;public interface VariationRepo {
+package com.dandelion.backend.repositories;
+
+import com.dandelion.backend.entities.Variation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface VariationRepo extends JpaRepository<Variation, Long> {
+    List<Variation> findByCategory_NameIgnoreCase(String name);
+
+
 }
