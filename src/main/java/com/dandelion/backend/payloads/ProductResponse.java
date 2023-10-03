45409eed8dee584a6165047e1cbb3d3a778075e5
ldphat99 <ldphat99@gmail.com>
2023-10-03 22:18:45 +0700

    ADD: Pagination for products

--- src/main/java/com/dandelion/backend/payloads/ProductResponse.java
+++ src/main/java/com/dandelion/backend/payloads/ProductResponse.java
@@ -1,2 +1,21 @@
-package com.dandelion.backend.payloads;public class ProductResponse {
+package com.dandelion.backend.payloads;
+
+import com.dandelion.backend.payloads.dto.ProductDTO;
+import lombok.*;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ProductResponse {
+
+    private List<ProductDTO> content;
+    private int pageNumber;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean lastPage;
 }
