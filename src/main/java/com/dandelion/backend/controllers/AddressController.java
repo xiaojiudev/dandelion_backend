4386f9ae53908823cfee5afb14f33aa70f02df7c
ldphat99 <ldphat99@gmail.com>
2023-10-05 23:23:10 +0700

    UPDATE: change suitable endpoint name

--- src/main/java/com/dandelion/backend/controllers/AddressController.java
+++ src/main/java/com/dandelion/backend/controllers/AddressController.java
@@ -1,26 +1,67 @@
 package com.dandelion.backend.controllers;
 
 
+import com.dandelion.backend.payloads.ApiResponse;
 import com.dandelion.backend.payloads.dto.AddressDTO;
 import com.dandelion.backend.services.AddressService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
 @AllArgsConstructor
 public class AddressController {
 
-    @Autowired
-    private AddressService addressService;
+    private final AddressService addressService;
 
-    @PostMapping("/address/{userId}")
+    @PostMapping("/user/{userId}/address")
     public ResponseEntity<AddressDTO> createAddress(
             @PathVariable("userId") Long userId,
-            @RequestBody AddressDTO addressDTO) {
-        return new ResponseEntity<>(addressService.createAddress(userId, addressDTO), HttpStatus.CREATED);
+            @RequestBody AddressDTO addressRequest) {
+        return new ResponseEntity<>(addressService.createAddress(userId, addressRequest), HttpStatus.CREATED);
     }
+
+    @PostMapping("/user/{userId}/address/{addressId}")
+    public ResponseEntity<ApiResponse> setDefaultAddress(
+            @PathVariable("userId") Long userId,
+            @PathVariable("addressId") Long addressId) {
+
+        addressService.setDefaultAddress(userId, addressId);
+
+        return new ResponseEntity<>(new ApiResponse("Set default address successfully", true), HttpStatus.OK);
+    }
+
+    @PutMapping("/user/{userId}/address/{addressId}")
+    public ResponseEntity<AddressDTO> updateAddress(
+            @PathVariable("userId") Long userId,
+            @PathVariable("addressId") Long addressId,
+            @RequestBody AddressDTO addressRequest) {
+        return new ResponseEntity<>(addressService.updateAddress(userId, addressId, addressRequest), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/user/{userId}/address/{addressId}")
+    public ResponseEntity<ApiResponse> deleteAddress(
+            @PathVariable("userId") Long userId,
+            @PathVariable("addressId") Long addressId) {
+
+        addressService.deleteAddress(userId, addressId);
+
+        return new ResponseEntity<>(new ApiResponse("Address is deleted successfully", true), HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{userId}/address")
+    public ResponseEntity<List<AddressDTO>> getAllAddress(@PathVariable("userId") Long userId) {
+        return new ResponseEntity<>(addressService.getAllAddress(userId), HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{userId}/address/default")
+    public ResponseEntity<AddressDTO> getDefaultAddress(@PathVariable("userId") Long userId) {
+        return new ResponseEntity<>(addressService.getDefaultAddress(userId), HttpStatus.OK);
+    }
+
+
 }
