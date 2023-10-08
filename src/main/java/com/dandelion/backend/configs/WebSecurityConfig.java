770212f090b6200d73065a3f033ec0115f45eca8
ldphat99 <ldphat99@gmail.com>
2023-10-08 11:09:50 +0700

    UPDATE: allow cors

--- src/main/java/com/dandelion/backend/configs/WebSecurityConfig.java
+++ src/main/java/com/dandelion/backend/configs/WebSecurityConfig.java
@@ -2,24 +2,46 @@ package com.dandelion.backend.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests(configurer ->
-                configurer
+        http
+                .authorizeHttpRequests(configurer -> configurer
                         .anyRequest()
                         .permitAll()
-        );
-
-        // Disable Cross Site Request Forgery
-        http.csrf(csrf -> csrf.disable());
-
+                )
+                .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults());
+        
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*"); // You can restrict this to specific origins
+        configuration.addAllowedMethod(HttpMethod.GET);
+        configuration.addAllowedMethod(HttpMethod.POST);
+        configuration.addAllowedMethod(HttpMethod.PUT);
+        configuration.addAllowedMethod(HttpMethod.DELETE);
+        configuration.addAllowedHeader("*"); // You can restrict this to specific headers
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
